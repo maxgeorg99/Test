619d2838f7692f92196e49e9693da19840b38bc2
maxgeorg99 <56111941+maxgeorg99@users.noreply.github.com>
2020-09-17 00:25:49 +0200

    Update skript.js

--- skript.js
+++ skript.js
@@ -71,7 +71,6 @@
 
     // show number of correct answers out of total
     resultsContainer.innerHTML = `Sie haben ${numCorrect} von ${myQuestions.length} Symptomen`;
-    displayTrafficLight();
   }
 
   function showSlide(n) {
